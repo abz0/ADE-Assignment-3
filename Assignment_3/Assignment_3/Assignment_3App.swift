38b7c5af04690ed2b23062bc7eacd080ff867725
abz0 <143391418+abz0@users.noreply.github.com>
2026-04-09 19:18:22 +1000

    initial_xcode

---
+++ Assignment_3/Assignment_3/Assignment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_3App.swift
+//  Assignment_3
+//
+//  Created by Abby on 9/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment_3/Assignment_3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment_3
+//
+//  Created by Abby on 9/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
