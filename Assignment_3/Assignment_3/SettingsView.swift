1829eb3de13c94f062aefc1d09f080c20da33fc9
Elissa348957 <Elissa.Miraziz@student.uts.edu.au>
2026-04-24 03:03:07 +1000

    Update SettingsView.swift

--- Assignment_3/Assignment_3/SettingsView.swift
+++ Assignment_3/Assignment_3/SettingsView.swift
@@ -1,63 +1,66 @@
 //
 //  SettingsView.swift
-//  
+//
 //
 //  Created by Elissa Miraziz (School) on 24/4/2026.
 //
 
 import SwiftUI
-@State private var highScore: Int = 0
+
 struct SettingsView: View {
+    @State private var highScore: Int = 0
     var body: some View {
-    /*    Text("Choose Topic")
-        Menu {
+        VStack{
+            Text("Choose Topic")
+            Menu {
+                Button {
+                } label: {
+                    Text("Topic 1")
+                }
+                .padding()
+                .clipShape(Capsule())
+                .foregroundColor(.black)
+                .background(Color.gray)
+                
+                Button {
+                } label: {
+                    Text("Topic 2")
+                }
+                Button {
+                } label: {
+                    Text("Topic 3")
+                }
+            } label: {
+                Text("Choose Topic")
+            }
+            
+            Spacer()
+            
+            Text("Difficulty Selection")
             Button {
             } label: {
-                Text("Topic 1")
+                Text("Easy: Levels 1-3")
             }
-            .padding()
-            .clipShape(Capsule())
-            .foregroundColor(.black)
-            .backgroundColor(.gray)
+            Button {
+            } label: {
+                Text("Medium: Levels 2-4")
+            }
+            Button {
+            } label: {
+                Text("Hard: Levels 3-5")
+            }
+            
+            Spacer()
+            
+            Text("High Score: \(highScore)")
+            
+            Spacer()
             
             Button {
             } label: {
-                Text("Topic 2")
+                Text("Start")
             }
-            Button {
-            } label: {
-                Text("Topic 3")
-            }
-        } label: {
-             Text("Choose Topic")
         }
-        
-        Spacer()
-        
-        Text("Difficulty Selection")
-        Button {
-        } label: {
-            Text("Easy: Levels 1-3")
-        }
-        Button {
-        } label: {
-            Text("Medium: Levels 2-4")
-        }
-        Button {
-        } label: {
-            Text("Hard: Levels 3-5")
-        }
-        
-        Spacer()
-        
-        Text("High Score: \(highScore)")
-        
-        Spacer()
-        
-        Button {
-        } label: {
-            Text("Start")
-        }*/
     }
 }
 
